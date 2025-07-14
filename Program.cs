02794030603c2fb9ce02643e6ad8a89dce1a9dd2
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-07-14 11:28:15 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniBankProject_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
